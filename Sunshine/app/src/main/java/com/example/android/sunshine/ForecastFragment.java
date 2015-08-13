ec61adf08d3b7ae526de595f6b72f1626a4d1377
Kartik Narayanan <kartik.narayanan@gmail.com>
2015-08-13 17:21:56 +0530

    adding onItemClickListener

--- Sunshine/app/src/main/java/com/example/android/sunshine/ForecastFragment.java
+++ Sunshine/app/src/main/java/com/example/android/sunshine/ForecastFragment.java
@@ -12,8 +12,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -89,6 +91,13 @@ public class ForecastFragment extends Fragment {
 
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         ListView weekview = (ListView) rootView.findViewById(R.id.listview_forecast);
+        weekview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getActivity(), mForecastAdapter.getItem(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+
         weekview.setAdapter(mForecastAdapter);
         return rootView;
     }
@@ -280,6 +289,7 @@ public class ForecastFragment extends Fragment {
             }
         }
     }
+
 
 }
 
