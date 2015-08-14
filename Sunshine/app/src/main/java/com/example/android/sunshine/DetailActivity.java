a3cc5749e008d41315796b47cdd7071d94028ba4
Kartik Narayanan <kartik.narayanan@gmail.com>
2015-08-14 16:47:59 +0530

    implemented shareactionprovider

--- Sunshine/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ Sunshine/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -5,27 +5,25 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class DetailActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-        Intent intent = getIntent();
-        Toast.makeText(getApplicationContext(), intent.getStringExtra(Intent.EXTRA_TEXT), Toast.LENGTH_SHORT);
-        TextView textView = (TextView) findViewById(R.id.daily_forecast);
-        textView.setText(intent.getStringExtra(Intent.EXTRA_TEXT).toString());
+
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_detail, menu);
+
         return true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
--- Sunshine/app/src/main/java/com/example/android/sunshine/DetailActivityFragment.java
+++ Sunshine/app/src/main/java/com/example/android/sunshine/DetailActivityFragment.java
@@ -1,22 +1,54 @@
 package com.example.android.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class DetailActivityFragment extends Fragment {
-
+    private String mDetailForecast = "";
     public DetailActivityFragment() {
+        setHasOptionsMenu(true);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+        Intent intent = getActivity().getIntent();
+        mDetailForecast = intent.getStringExtra(Intent.EXTRA_TEXT).toString();
+        TextView textView = (TextView) rootView.findViewById(R.id.daily_forecast);
+        textView.setText(mDetailForecast);
+        return rootView;
+    }
+
+    private Intent createShareForecastIntent() {
+        Intent mShareIntent = new Intent(Intent.ACTION_SEND)
+                .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                .setType("text/plain")
+                .putExtra(Intent.EXTRA_TEXT, mDetailForecast + " #SunshineApp");
+        return mShareIntent;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.detailfragment, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        ShareActionProvider mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        if (mShareActionProvider != null) {
+
+            mShareActionProvider.setShareIntent(createShareForecastIntent());
+        }
     }
 }
