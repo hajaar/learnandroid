106db5a4c4d76d2a9cf148aeca10bd423175bff5
Kartik Narayanan <kartik.narayanan@gmail.com>
2015-10-27 19:59:01 +0530

    added showing graph on start. converted int to long

--- DiabetesMonitoring/app/src/main/java/com/kartik/diabetesmonitoring/MainActivity.java
+++ DiabetesMonitoring/app/src/main/java/com/kartik/diabetesmonitoring/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -30,7 +29,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         insulinReadingDataSource = new InsulinReadingDataSource(this);
         insulinReadingDataSource.open();
-        Log.d("mainactivity", "oncreate");
         drawGraphOnStart();
 
     }
