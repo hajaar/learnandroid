32e064bdcf1a126ba391051c5cd09aeb780c63fd
Kartik Narayanan <kartik.narayanan@gmail.com>
2016-10-16 10:43:55 +0530

    initial commit

--- MathsPlayforKids/app/src/main/java/com/skva/mathsplayforkids/MainActivity.java
+++ MathsPlayforKids/app/src/main/java/com/skva/mathsplayforkids/MainActivity.java
@@ -1,7 +1,9 @@
 package com.skva.mathsplayforkids;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -10,6 +12,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -34,6 +37,7 @@ public class MainActivity extends Activity implements AdapterView.OnItemSelected
         final Button button1 = (Button) findViewById(R.id.button1);
         button1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+
                 a = Integer.parseInt(button1.getText().toString());
                 validateAnswer();
                 Log.d("BUTTON1onClick","a: "+a);
@@ -66,14 +70,16 @@ public class MainActivity extends Activity implements AdapterView.OnItemSelected
         final Button button5 = (Button) findViewById(R.id.skip);
         button5.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                total_attempts += 1;
                 generateQuestion();
+
                 Log.d("SKIP","skip ");
 
             }
         });
         spinner1 = (Spinner) findViewById(R.id.spinner1);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.game_list, android.R.layout.simple_spinner_item);
+                R.array.game_list, R.layout.custom_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner1.setAdapter(adapter);
 
@@ -114,11 +120,11 @@ public class MainActivity extends Activity implements AdapterView.OnItemSelected
                 q=q-2;
             }
             Log.d("generateQuestion", "q : " +q);
-            question = "Which number is (greater than) >  " + q;
+            question = " >  " + q + " ? ";
             choiceArray[0] = r.nextInt(q-difficulty_min)+difficulty_min;
             choiceArray[1] = r.nextInt(q-difficulty_min)+difficulty_min;
             choiceArray[2] = r.nextInt(q-difficulty_min)+difficulty_min;
-            choiceArray[3] = r.nextInt(difficulty_max-q-1)+q+1;
+            choiceArray[3] = r.nextInt(difficulty_max-q)+q+1;
             Log.d("generateQuestion", "a1: " +choiceArray[0]+" a2: " +choiceArray[1] + " a3: " +choiceArray[2] + " a4: " +choiceArray[3]);
         }
         if ((game_type == 1) && (difficulty == 0)) {
@@ -129,78 +135,75 @@ public class MainActivity extends Activity implements AdapterView.OnItemSelected
                 q=q-2;
             }
             Log.d("generateQuestion", "q : " +q);
-            question = "Which number is (less than) <  " + q;
-            choiceArray[0] = r.nextInt(difficulty_max-q-1)+q+1;
-            choiceArray[1] = r.nextInt(difficulty_max-q-1)+q+1;
-            choiceArray[2] = r.nextInt(difficulty_max-q-1)+q+1;
+            question = " < " + q + " ? ";
+            choiceArray[0] = r.nextInt(difficulty_max-q)+q+1;
+            choiceArray[1] = r.nextInt(difficulty_max-q)+q+1;
+            choiceArray[2] = r.nextInt(difficulty_max-q)+q+1;
             choiceArray[3] = r.nextInt(q-difficult_min)+difficult_min;
             Log.d("generateQuestion", "a1: " +choiceArray[0]+" a2: " +choiceArray[1] + " a3: " +choiceArray[2] + " a4: " +choiceArray[3]);
         }
         if ((game_type == 2) && (difficulty == 0)) {
             int difficulty_max = 50;
-            int difficult_min = 5;
-            int mid = (int)(difficult_min+difficulty_max)/2;
-            int p = r.nextInt(mid-difficult_min)+difficult_min;
-            int q = r.nextInt(difficulty_max-mid)+mid;
+            int difficulty_min = 5;
+            int q = r.nextInt(difficulty_max-difficulty_min)+difficulty_min+1;
+            if (q==difficulty_max) {
+                q=q-2;
+            }
+            int p = q+2;
             Log.d("generateQuestion", "p: "+p+" q : " +q);
-            question = "Which number is between " +p + " & " + q;
-            choiceArray[0] = r.nextInt(p)+difficult_min;
-            choiceArray[1] = r.nextInt(difficulty_max-q-1)+q+1;
-            choiceArray[2] = r.nextInt(difficulty_max-q-1)+q+1;
-            choiceArray[3] = r.nextInt(q-p)+p;
+            question = " " +q + " ___ " + p + " ?";
+            choiceArray[0] = r.nextInt(p)+difficulty_min;
+            choiceArray[1] = r.nextInt(q)+difficulty_min;
+            choiceArray[2] = r.nextInt(q)+p;
+            choiceArray[3] = q+1;
             Log.d("generateQuestion", "a1: " +choiceArray[0]+" a2: " +choiceArray[1] + " a3: " +choiceArray[2] + " a4: " +choiceArray[3]);
         }
         if ((game_type == 3) && (difficulty == 0)) {
             int difficulty_max = 10;
-            int difficult_min = 0;
+            int difficult_min = 1;
             int mid = (int)(difficult_min+difficulty_max)/2;
             int p = r.nextInt(mid-difficult_min)+difficult_min;
             int q = r.nextInt(difficulty_max-mid)+mid;
             Log.d("generateQuestion", "p: "+p+" q : " +q);
-            question = "What is " +p + " + " + q;
+            question = p + " + " + q+" ?";
             choiceArray[0] = p+q+r.nextInt(difficulty_max-difficult_min)+1;
-            choiceArray[1] = p+q+r.nextInt(difficulty_max)-1;
+            choiceArray[1] = q-p+r.nextInt(difficulty_max)-1;
             choiceArray[2] = (p+q)/2;
             choiceArray[3] = p+q;
             Log.d("generateQuestion", "a1: " +choiceArray[0]+" a2: " +choiceArray[1] + " a3: " +choiceArray[2] + " a4: " +choiceArray[3]);
         }
         if ((game_type == 4) && (difficulty == 0)) {
             int difficulty_max = 10;
-            int difficult_min = 0;
+            int difficult_min = 1;
             int mid = (int)(difficult_min+difficulty_max)/2;
             int p = r.nextInt(mid-difficult_min)+difficult_min;
             int q = r.nextInt(difficulty_max-mid)+mid;
             Log.d("generateQuestion", "p: "+p+" q : " +q);
-            question = "What is " +q + " - " + p;
+            question = q + " - " + p+" ?";
             choiceArray[0] = q-p+r.nextInt(difficulty_max-difficult_min)+1;
-            choiceArray[1] = q-p+r.nextInt(difficulty_max)-1;
+            choiceArray[1] = p+q;
             choiceArray[2] = (q-p)/2;
             choiceArray[3] = q-p;
             Log.d("generateQuestion", "a1: " +choiceArray[0]+" a2: " +choiceArray[1] + " a3: " +choiceArray[2] + " a4: " +choiceArray[3]);
         }
         updateView();
-        total_attempts += 1;
+
 
     }
 
     private void validateAnswer() {
+        boolean success = false;
+        total_attempts += 1;
         if (a==choiceArray[3]) {
+            Log.d("validateAnswer","validation failed");
+            success=true;
+            showSucessFailure(success);
             updateScore(1);
             generateQuestion();
         } else {
             Log.d("validateAnswer","validation failed");
-            LayoutInflater inflater = getLayoutInflater();
-            View layout = inflater.inflate(R.layout.custom_toast_success,
-                    (ViewGroup) findViewById(R.id.custom_toast_container));
-
-            TextView text = (TextView) layout.findViewById(R.id.text);
-            text.setText("Try Again");
-
-            Toast toast = new Toast(getApplicationContext());
-            toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
-            toast.setDuration(Toast.LENGTH_SHORT);
-            toast.setView(layout);
-            toast.show();
+            success = false;
+            showSucessFailure(success);
         }
     }
 
@@ -214,7 +217,7 @@ public class MainActivity extends Activity implements AdapterView.OnItemSelected
             }
             tempArray[j] = choiceArray[i];
         }
-        ((TextView) findViewById(R.id.score)).setText(" "+ score);
+        ((TextView) findViewById(R.id.score)).setText(" Score: "+ score + "/" + total_attempts);
         ((TextView) findViewById(R.id.question)).setText(question);
         ((Button) findViewById(R.id.button1)).setText(Integer.toString(tempArray[0]));
         ((Button) findViewById(R.id.button2)).setText(Integer.toString(tempArray[1]));
@@ -255,4 +258,47 @@ public class MainActivity extends Activity implements AdapterView.OnItemSelected
                 break;
         }
     }*/
+
+    private void showSucessFailure(boolean success) {
+        int sound, image, time;
+        String text;
+        if (success) {
+            sound = R.raw.rightapplause;
+            image = R.drawable.happysmiley;
+            text = "You Are Right!";
+            time = 1000;
+
+        } else {
+            sound = R.raw.wrongbuzzer;
+            image = R.drawable.worriedsmiley;
+            text = "Try Again";
+            time = 500;
+        }
+        final MediaPlayer mp = MediaPlayer.create(getApplicationContext(), sound);
+
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.custom_toast,
+                (ViewGroup) findViewById(R.id.custom_toast_container));
+
+        ImageView toast_image = (ImageView) layout.findViewById(R.id.toast_image);
+        toast_image.setImageResource(image);
+
+        TextView toast_message = (TextView) layout.findViewById(R.id.toast_message);
+        toast_message.setText(text);
+
+        final Toast toast = new Toast(getApplicationContext());
+        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setView(layout);
+        mp.start();
+        toast.show();
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                toast.cancel();
+                mp.stop();
+            }
+        }, time);
+    }
 }
